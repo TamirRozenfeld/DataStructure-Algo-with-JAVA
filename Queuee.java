27549b3f462e13943d4081ed3f977c541380f572
TamirRozenfeld <tamirrozenfeld1@gmail.com>
2022-08-29 23:08:38 +0300

    Queue

---
+++ Queuee.java
@@ -0,0 +1,26 @@
+import java.util.Queue;
+import java.util.LinkedList;
+public class Queuee{
+	
+	public static void main(String[] args) {
+		
+		
+		Queue<String> queue = new LinkedList<String>();
+
+		//System.out.println(queue.isEmpty());
+
+		queue.offer("Karen");
+		queue.offer("Chad");
+		queue.offer("Steve");
+		queue.offer("Harold");
+		
+		System.out.println(queue.contains("Harold"));
+		//System.out.println(queue.size());
+		//System.out.println(queue.isEmpty());
+		//System.out.println(queue.peek());
+		//queue.poll();
+		//queue.poll();
+
+		System.out.println(queue);
+	}
+}
